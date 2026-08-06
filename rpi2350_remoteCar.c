80b2d9b4cd47fb26bcc36d83561fbc0ecc2047b5
inveh <inveh.info@gmail.com>
2026-08-06 21:17:04 +0530

    Add initial project files for Raspberry Pi Pico remote car
    - Create .gitignore to exclude build files
    - Add VS Code configuration files: c_cpp_properties.json, cmake-kits.json, extensions.json, launch.json, settings.json, tasks.json
    - Implement CMakeLists.txt for project setup
    - Add blink.pio for LED control
    - Include pico_sdk_import.cmake for SDK integration
    - Implement main functionality in rpi2350_remoteCar.c for blinking LED and Wi-Fi initialization

---
+++ rpi2350_remoteCar.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "pico/cyw43_arch.h"
+
+#include "blink.pio.h"
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+
+    printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+}
+
+
+
+
+int main()
+{
+    stdio_init_all();
+
+    // Initialise the Wi-Fi chip
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed\n");
+        return -1;
+    }
+
+    // PIO Blinking example
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &blink_program);
+    printf("Loaded program at %d\n", offset);
+    
+    #ifdef PICO_DEFAULT_LED_PIN
+    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
+    #else
+    blink_pin_forever(pio, 0, offset, 6, 3);
+    #endif
+    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+
+    // Example to turn on the Pico W LED
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
