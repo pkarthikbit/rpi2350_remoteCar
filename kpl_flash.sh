dc7529a58bd1496e17229699940c3cd4a96efef1
Inveh <inveh.info@gmail.com>
2026-08-26 21:50:05 +0530

    picotool installed

--- kpl_flash.sh
+++ kpl_flash.sh
@@ -1,1 +1,1 @@
-sudo /home/karthik/pico-sdk/picotool/2.1.1/picotool/picotool load /home/karthik/Documents/rpi2350_remoteCar/_build/rpi2350_rc.elf -fx
+sudo /home/karthik/.local/bin/picotool load /home/karthik/Documents/rpi2350_remoteCar/_build/rpi2350_rc.elf -fx
