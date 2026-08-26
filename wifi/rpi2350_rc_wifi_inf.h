319a29f9550a1cb610e1abefa63895bdcfd34d97
Inveh <inveh.info@gmail.com>
2026-08-26 20:54:07 +0530

    wifi code cleaned up

--- wifi/rpi2350_rc_wifi_inf.h
+++ wifi/rpi2350_rc_wifi_inf.h
@@ -14,22 +14,12 @@
 #include "pico/unique_id.h"
 
 /* for Hardware */
-#include "hardware/gpio.h"
-#include "hardware/irq.h"
-#include "hardware/adc.h"
+
 
 /* for FreeRTOS */
 #include "FreeRTOS.h"
 #include "task.h"
 
-/* for LwIP */
-#include "lwip/apps/mqtt.h"
-#include "lwip/apps/mqtt_priv.h" // needed to set hostname
-#include "lwip/dns.h"
-#include "lwip/altcp_tls.h"
-
-#include "lwipopts.h"
-#include "mbedtls_config.h"
 
 #include "rpi2350_rc_ble_pub.h"
 
